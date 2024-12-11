d68e69e3bba26f7401b839918f2e8a1bae6eb653
Mariorlando96 <ormario1996@gmail.com>
2024-12-11 12:21:14 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function getRandomElement(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
+function loadExcuse() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+    let excuse = getRandomElement(who) + " " + getRandomElement(action) + " " + getRandomElement(what) + " " + getRandomElement(when);
+    document.getElementById('excuse').innerHTML = excuse;
+}
