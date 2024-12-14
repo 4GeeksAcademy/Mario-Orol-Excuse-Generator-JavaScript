d1817f3139f4cf763c548890a06d1c7ab527b144
Mariorlando96 <ormario1996@gmail.com>
2024-12-14 10:45:38 -0500

    Moving the .js and html to the src folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function () {
+  function getRandomElement(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateRandomExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying",
+    ];
+
+    let excuse = `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}`
+    
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  generateRandomExcuse();
 };
